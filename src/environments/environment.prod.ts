bb77cfde849e57ee9556cc698a1fc0917723d678
ryanbeevers <beeversryan@gmail.com>
2025-06-24 23:59:35 -0400

    Updated api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://my-resume-app-backend-1.onrender.com'
+  apiBaseUrl: 'https://54ff-2600-2b00-6b25-cb00-7209-5fc9-ec6a-16bf.ngrok-free.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://127.0.0.1:5000'
+  apiBaseUrl: 'https://54ff-2600-2b00-6b25-cb00-7209-5fc9-ec6a-16bf.ngrok-free.app'
 };
 
