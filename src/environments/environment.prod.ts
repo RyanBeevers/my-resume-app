b838f39e300cdf3fced0866eebdb45ebfc306d36
ryanbeevers <beeversryan@gmail.com>
2025-06-25 07:23:26 -0400

    Added static url for backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://54ff-2600-2b00-6b25-cb00-7209-5fc9-ec6a-16bf.ngrok-free.app'
+  apiBaseUrl: 'https://smoothly-stable-ray.ngrok-free.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://54ff-2600-2b00-6b25-cb00-7209-5fc9-ec6a-16bf.ngrok-free.app'
+  apiBaseUrl: 'https://smoothly-stable-ray.ngrok-free.app'
 };
 
